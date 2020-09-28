5c7be84f2ff37db2c803e6249761d041069d7230
Shimon Magal <shimon.magal@gmail.com>
2020-09-29 02:15:14 +0300

    yo

--- src/components/FormBuilder/ToolbarItem/ToolbarItem.js
+++ src/components/FormBuilder/ToolbarItem/ToolbarItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classNames from "classnames";
-import { DragSource, DropTarget } from "react-dnd";
+import { DragSource } from "react-dnd";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { addItem } from "../../../actions/formBuilderActions";
@@ -19,69 +19,61 @@ const spec = {
   endDrag(props, monitor, component) {
     if (!monitor.didDrop()) return; // return if not dropped in the Preview component
     props.addItem(props.data);
+	
+    const dragIndex = monitor.getItem().index;
+	const hoverIndex = props.index;
+	
+	// Don't replace items with themselves
+	if (isEqual(dragIndex, hoverIndex)) {
+		return;
+    }
+	
+	// Determine rectangle on screen
+	const hoverBoundingRect = findDOMNode(
+	  component
+    ).getBoundingClientRect();
+
+  // Get vertical middle
+  const hoverMiddleY =
+	  (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+  // Determine mouse position
+  const clientOffset = monitor.getClientOffset();
+
+  // Get pixels to the top
+  const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+  // Only perform the move when the mouse has crossed half of the items height
+  // When dragging downwards, only move when the cursor is below 50%
+  // When dragging upwards, only move when the cursor is above 50%
+  // Dragging downwards
+  if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+	  return;
+  }
+
+  // Dragging upwards
+  if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+	  return;
+  }
+
+  console.log("debug");
+  console.log(dragIndex);
+  console.log(hoverIndex);
+  console.log(monitor.getItem());
+  console.log(props);
+  console.log(component);
+  console.log("done debug----");
+
+  // Time to actually perform the action
+  props.dragItem(dragIndex, hoverIndex);
+
+  // Note: we're mutating the monitor item here!
+  // Generally it's better to avoid mutations,
+  // but it's good here for the sake of performance
+  // to avoid expensive index searches.
+  monitor.getItem().index = hoverIndex;
   }
 };
-
-const specTarget = {
-	hover(props, monitor, component)
-	{
-		const dragIndex = monitor.getItem().index;
-		const hoverIndex = props.index;
-		
-		// Don't replace items with themselves
-		if (isEqual(dragIndex, hoverIndex))
-		{
-			return;
-		}
-		
-		// Determine rectangle on screen
-		const hoverBoundingRect = findDOMNode(
-			component
-		).getBoundingClientRect();
-		
-		// Get vertical middle
-		const hoverMiddleY =
-			(hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-		
-		// Determine mouse position
-		const clientOffset = monitor.getClientOffset();
-		
-		// Get pixels to the top
-		const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-		
-		// Only perform the move when the mouse has crossed half of the items height
-		// When dragging downwards, only move when the cursor is below 50%
-		// When dragging upwards, only move when the cursor is above 50%
-		// Dragging downwards
-		if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY)
-		{
-			return;
-		}
-		
-		// Dragging upwards
-		if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY)
-		{
-			return;
-		}
-		
-		console.log("debug");
-		console.log(dragIndex);
-		console.log(hoverIndex);
-		console.log(monitor.getItem());
-		console.log(props);
-		console.log(component);
-		console.log("done debug----");
-		
-		// Time to actually perform the action
-		props.dragItem(dragIndex, hoverIndex);
-		
-		// Note: we're mutating the monitor item here!
-		// Generally it's better to avoid mutations,
-		// but it's good here for the sake of performance
-		// to avoid expensive index searches.
-		monitor.getItem().index = hoverIndex;
-	}
-}
 
 const collect = (connect, monitor) => ({
   connectDragSource: connect.dragSource(),
@@ -121,6 +113,5 @@ export default compose(
       addItem
     }
   ),
-  DragSource(type, spec, collect),
-  DropTarget(type, specTarget, collect)
+  DragSource(type, spec, collect)
 )(ToolbarItem);
