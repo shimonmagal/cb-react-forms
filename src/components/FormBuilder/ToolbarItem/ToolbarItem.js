a54f27b171322b3bbc65c66c9bd4c0488a789126
Shimon Magal <shimon.magal@gmail.com>
2020-09-29 01:45:42 +0300

    more debug info

--- src/components/FormBuilder/ToolbarItem/ToolbarItem.js
+++ src/components/FormBuilder/ToolbarItem/ToolbarItem.js
@@ -4,6 +4,8 @@ import { DragSource } from "react-dnd";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { addItem } from "../../../actions/formBuilderActions";
+import isEqual from "lodash/isEqual";
+import {findDOMNode} from "react-dom";
 
 // type, spec and collect are the paramters to the DragSource HOC
 const type = props => "items";
@@ -14,7 +16,65 @@ const spec = {
       item: props.data.key
     };
   },
-  endDrag(props, monitor, component) {
+	hover(props, monitor, component)
+	{
+		const dragIndex = monitor.getItem().index;
+		const hoverIndex = props.index;
+		
+		// Don't replace items with themselves
+		if (isEqual(dragIndex, hoverIndex))
+		{
+			return;
+		}
+		
+		// Determine rectangle on screen
+		const hoverBoundingRect = findDOMNode(
+			component
+		).getBoundingClientRect();
+		
+		// Get vertical middle
+		const hoverMiddleY =
+			(hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+		
+		// Determine mouse position
+		const clientOffset = monitor.getClientOffset();
+		
+		// Get pixels to the top
+		const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+		
+		// Only perform the move when the mouse has crossed half of the items height
+		// When dragging downwards, only move when the cursor is below 50%
+		// When dragging upwards, only move when the cursor is above 50%
+		// Dragging downwards
+		if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY)
+		{
+			return;
+		}
+		
+		// Dragging upwards
+		if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY)
+		{
+			return;
+		}
+		
+		console.log("debug");
+		console.log(dragIndex);
+		console.log(hoverIndex);
+		console.log(monitor.getItem());
+		console.log(props);
+		console.log(component);
+		console.log("done debug----");
+		
+		// Time to actually perform the action
+		props.dragItem(dragIndex, hoverIndex);
+		
+		// Note: we're mutating the monitor item here!
+		// Generally it's better to avoid mutations,
+		// but it's good here for the sake of performance
+		// to avoid expensive index searches.
+		monitor.getItem().index = hoverIndex;
+	},
+		endDrag(props, monitor, component) {
     if (!monitor.didDrop()) return; // return if not dropped in the Preview component
     props.addItem(props.data);
   }
