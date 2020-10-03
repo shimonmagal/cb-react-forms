d2f9edb90a6bb4d0ff5c86b2dfd35d8c4095fd7e
Shimon Magal <shimon.magal@gmail.com>
2020-10-03 16:41:22 +0300

    a

--- src/components/FormBuilder/Preview/index.js
+++ src/components/FormBuilder/Preview/index.js
@@ -104,9 +104,9 @@ class Preview extends Component {
               previewItems.map((item, i) => (
                 <FormInputs
                   index={i}
-                  item={item ? item: {}}
-                  id={item ? item.id: 1}
-                  key={item ? item.id: 4}
+                  item={item ? item: {id : i}}
+                  id={item ? item.id: i}
+                  key={item ? item.id: i}
                   dragItem={dragItem}
                   removeItem={removeItem}
                   showEditor={showEditor}
